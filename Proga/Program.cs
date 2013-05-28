385e8b420a82cb9347cab9bd89de5e722e7b3fdf
Kellerman23 <Alekseev.valen@mail.ru>
2013-05-28 21:28:57 +1000

    The second task is added

--- Proga/Program.cs
+++ Proga/Program.cs
@@ -14,11 +14,39 @@ namespace integral
             s2 = (b / 2) * Math.Sqrt(b * b + 1) + 0.5 * Math.Log(b + Math.Sqrt(b * b + 1));
             return s1 - s2;
         }
+        static double sortirovka(double[] a, double n)
+        {
+            double k;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    if (a[i] > a[j])
+                    {
+                        k = a[j];
+                        a[j] = a[i];
+                        a[i] = k;
+                    }
+            return 0;
+        }
         static void Main(string[] args)
         {
             double a;
             a = integsqrt(1, -1);
             Console.WriteLine("Значение интеграла: " + a);
+            Console.WriteLine("Введите количество элементов массива:");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+            double[] a1 = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                a1[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            sortirovka(a1, n);
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a1[i] + " ");         
+            }
+        }
         }
     }
-}
+
